62b9716b0ec33eabb27969427aa8085e954cbc59
DeChrish <dechrish@hotmail.com>
2015-11-21 11:22:24 -0800

    better code, i guess

--- src/hi.js
+++ src/hi.js
@@ -1,39 +1,11 @@
 var request = require('request');
 var C = {};
-C.requestFunctionSample= function(){
-	C.requestFunction();
-};
+
 C.requestFunction = function(){
-	request('http://www.google.com', function (error, response, body) {
+	return request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage.
+    console.log(response.statusCode) 
   }
 })
 }
 module.exports = C;
-
-
-C.requestFunctionSample();
-
-// module.exports = {
-// 	hi:function (){ 
-//    return 'hi hi';
-// 	}
-// }
-
-// var C = {};
-// C.hi =  function(){
-// 	return 'hi hi';
-// }
-// module.exports = C;
-
-
-
-
-
-	// request('http://www.google.com', function (error, response, body) {
-	// 	console.log(error, response, body)
-	// 	if (!error && response.statusCode == 200) {
-	// 	  console.log(body) // Show the HTML for the Google homepage. 
-	// 	}
-	// })
