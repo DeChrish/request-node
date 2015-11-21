d356398701989cfad01f0b511bdd97a57f31d634
DeChrish <dechrish@hotmail.com>
2015-11-21 11:21:50 -0800

    better code, i guess

--- test/hi_spec.js
+++ test/hi_spec.js
@@ -1,22 +1,13 @@
-// var chai = require('chai');
 var assert = require('assert');
+var expect = require("chai").expect;
 
 var hi = require('../src/hi.js');
 
-var user = {
-    "username": "col622510010",
-    "password": "password7",
-     "label": "Testing",
-    "group": "Member"
-}
-
-// it("should have sso session" , function(){
-//   assert.equal(typeof hi, 'object');
-//   assert.equal(hi.hi(), 'hi hi');
-// });
-
 it("should have sso session" , function(done){
-  hi.requestFunctionSample();
-  done();
-  // hi.request();
+  hi.requestFunction(function(err) {
+                if(err) {
+                    return done(err);
+                }
+                done();
+            });
 });
